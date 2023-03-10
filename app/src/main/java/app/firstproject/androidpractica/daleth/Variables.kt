e8e538c2f4d8bb175c21a0ac9650c95a71ae651b
NanoDaleth <pedroallaucafer@gmail.com>
2023-03-10 01:21:40 -0500

    1

---
+++ app/src/main/java/app/firstproject/androidpractica/daleth/Variables.kt
@@ -0,0 +1,35 @@
+package app.firstproject.androidpractica.daleth
+
+fun main(){
+    //print("Daleth")
+
+    //varaiables numericas
+    val name="Nano"
+    //Int entero/real
+    val age:Int=30
+    //Long
+    val example:Long=80000
+    //float
+    val flotante:Float=32.5f
+    //double
+    val dobles:Double=3555.2222
+
+    //varaiables alfanumericas
+    //char
+    val charExample1:Char='Q'
+    val charExample2:Char='2'
+    val charExample3:Char='/'
+
+    //String
+    val stringExample:String="Nano"
+
+    //varaibles booleanas
+
+    //Boolean
+    val booleanExample1:Boolean=true
+    val booleanExample2:Boolean=false
+
+    print(stringExample)
+
+
+}
